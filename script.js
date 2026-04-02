91931af55dc6846cb37fa26e26900fa2420d4e81
omarrrahmeddd <omar42ahmed00@gmail.com>
2026-04-02 07:59:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ let logo=document.querySelector(".logo");
 let navAllLinks=document.querySelectorAll(".navigation a");
 let header=document.querySelector("header");
 let tittle=document.querySelectorAll(".tittle");
+let home=document.querySelector(".home");
 
 
 DarkMode.addEventListener("click",()=>{
     document.body.classList.toggle("DarkMode");
     if(document.body.classList.contains("DarkMode")){
         logo.style.color="white";
-        navAllLinks.forEach(link=>{
-            link.style.color="white";
-        });
+       
         DarkMode.style.color="black";
         header.style.backgroundColor="rgb(38, 31, 48)";
         tittle.forEach(t=>{
             t.style.color="white";
         });
+       
+       
 
     }else{
-        logo.style.color="blue";
-        navAllLinks.forEach(link=>{
-            link.style.color="blue";
-        });
+        
+        
+        logo.style.color="rgb(51, 32, 113)";
+       
         DarkMode.style.color="Grey";
         header.style.backgroundColor="rgb(236, 221, 221)";
         tittle.forEach(t=>{
